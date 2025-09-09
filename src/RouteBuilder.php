96b9b769940896ff0887283345767c17e44947ae
Seth Battis <sbattis@groton.org>
2025-09-09 15:38:15 -0400

    feat!: support constructor injection by making static members dynamic
    BREAKING: Static invocations of Dependencies::inject() and RouteBuilder::define()
    will now fail. Dependencies and RouteBuilder must be instantiated before the
    instance methods inject() or define(), respectively, can be called.

--- src/RouteBuilder.php
+++ src/RouteBuilder.php
@@ -10,24 +10,55 @@ use GrotonSchool\Slim\OAuth2\APIProxy\Actions\DeauthorizeAction;
 use GrotonSchool\Slim\OAuth2\APIProxy\Actions\OwnerAction;
 use GrotonSchool\Slim\OAuth2\APIProxy\Actions\ProxyAction;
 use GrotonSchool\Slim\OAuth2\APIProxy\Actions\RedirectAction;
+use GrotonSchool\Slim\OAuth2\APIProxy\Domain\AccessToken\AccessTokenFactory;
+use GrotonSchool\Slim\OAuth2\APIProxy\Domain\Provider\ProviderInterface;
 use Odan\Session\Middleware\SessionStartMiddleware;
+use Odan\Session\SessionInterface;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface;
 use Slim\Interfaces\RouteGroupInterface;
 
 class RouteBuilder implements RouteBuilderInterface
 {
-    public static function define(App $app, string $providerSlug = 'proxy'): RouteGroupInterface
+    public function __construct(
+        private ProviderInterface $provider,
+        private SessionInterface $session
+    ) {}
+
+    public function define(App $app): RouteGroupInterface
     {
-        $providerSlug = preg_replace('/^-?(.+)-?$/', '$1', preg_replace('/[^a-z0-9\-]+/', '-', $providerSlug));
-        return $app->group("/$providerSlug", function (RouteCollectorProxyInterface $api) {
-            $api->group("/login", function (RouteCollectorProxyInterface $login) {
-                $login->get('/authorize', AuthorizeAction::class);
-                $login->get('/redirect', RedirectAction::class);
-                $login->get('/deauthorize', DeauthorizeAction::class);
+        $providerSlug = preg_replace(
+            '/^-?(.+)-?$/',
+            '$1',
+            preg_replace(
+                '/[^a-z0-9\-]+/',
+                '-',
+                $this->provider->getSlug()
+            )
+        );
+        $provider = $this->provider;
+        $session = $this->session;
+        return $app->group("/$providerSlug", function (RouteCollectorProxyInterface $api) use ($provider, $session) {
+            $api->group("/login", function (RouteCollectorProxyInterface $login) use ($provider, $session) {
+                $login->get('/authorize', new AuthorizeAction(
+                    $provider,
+                    $session
+                ));
+                $login->get('/redirect', new RedirectAction(
+                    $provider,
+                    $session
+                ));
+                $login->get('/deauthorize', new DeauthorizeAction(
+                    new AccessTokenFactory($provider)
+                ));
             });
-            $api->get('/owner', OwnerAction::class);
-            $api->any('/proxy[/{path:.*}]', ProxyAction::class);
+            $api->get('/owner', new OwnerAction(
+                $provider
+            ));
+            $api->any('/proxy[/{path:.*}]', new ProxyAction(
+                $provider,
+                $session
+            ));
         })
             ->add(SessionStartMiddleware::class);
     }
